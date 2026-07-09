63095f1735fbdcb1dfafd89c518b70093a6999cc
george1786 <bronc2340@gmail.com>
2026-07-08 23:31:17 -0600

    first commit - spanish classes site

---
+++ src/app/app.ts
@@ -0,0 +1,16 @@
+import { Header} from './header/header';
+import { Home } from './home/home';
+import { Footer } from './footer/footer';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true, 
+  imports: [RouterOutlet, Header, Home, Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('moni-spanish-site');
+}

898b8094eb924052f87af6d08cfdffbdffa5733f
irving gonzalez <irving_a_gonzalez@hotmail.com>
2026-07-17 01:24:03 -0500

    add angular router and calendar component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Calendar } from './calendar/calendar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'calendar', component: Calendar },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,12 @@
-import { Header} from './header/header';
-import { Home } from './home/home';
+import { Header } from './header/header';
 import { Footer } from './footer/footer';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true, 
-  imports: [RouterOutlet, Header, Home, Footer],
+  standalone: true,
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

13f1e297a017f59b85eeab441de7db8189f5283b
george1786 <bronc2340@gmail.com>
2026-07-18 20:30:43 -0600

    google calendar

--- src/app/calendar/calendar.ts
+++ src/app/calendar/calendar.ts
@@ -9,11 +9,11 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   styleUrl: './calendar.css',
 })
 export class Calendar {
-  constructor(private readonly sanitizer: DomSanitizer) {}
+  constructor(private readonly sanitizer: DomSanitizer) { }
 
   // Replace this with your own public Google Calendar embed URL.
   private readonly googleCalendarEmbedUrl =
-    'https://calendar.google.com/calendar/embed?src=en.usa%23holiday%40group.v.calendar.google.com&ctz=America%2FChicago';
+    'https://calendar.google.com/calendar/embed?src=9f1aca63ac981964767516454a21c318133a54f001ea036ae34f9d75d0fe3885%40group.calendar.google.com&ctz=America%2FLos_Angeles'
 
   get googleCalendarSrc(): SafeResourceUrl {
     return this.sanitizer.bypassSecurityTrustResourceUrl(this.googleCalendarEmbedUrl);

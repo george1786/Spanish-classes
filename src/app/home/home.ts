1cecdb499784d4fffd5aa8cc6afcc9c2172e0fdf
george1786 <bronc2340@gmail.com>
2026-09-14 18:39:45 -0600

    Slideshow working

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +6,30 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnDestroy {
+  images: string[] = [
+    'slideshow/bogota.jpg',
+    'slideshow/madrid.jpg',
+    'slideshow/buenosaires.jpg',
+    'slideshow/pachuca.jpg',
+    'slideshow/machupichu.jpg',
+  ];
+
+  countries: string[] = ['Colombia', 'España', 'Argentina', 'México', 'Perú'];
+
+  currentIndex = signal(0);
+
+  private intervalId: any;
+
+  constructor() {
+    this.intervalId = setInterval(() => {
+      this.currentIndex.update(i => (i + 1) % this.images.length);
+    }, 14000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+}
+
+
